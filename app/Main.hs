c09e0350b4ecd736b1e7e6a35eca56917d8ed385
Rutger Broekhoff <rutger@viasalix.nl>
2022-03-08 13:01:22 +0100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,510 @@
+module Main where
+
+import Control.Applicative (Applicative (..))
+import Control.Monad (Monad (..))
+import Control.Monad.Writer.Lazy
+import Data.List (foldl', intercalate)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import qualified Data.Set as Set
+import System.Environment
+import System.Exit (exitFailure)
+import Text.Lare
+
+data DFA s a = DFA
+  { dfaA :: Set.Set a,
+    dfaQ :: Set.Set s,
+    dfaI :: s,
+    dfaF :: Set.Set s,
+    dfaD :: Map.Map (s, a) s
+  }
+
+data NFA s a = NFA
+  { nfaA :: Set.Set a,
+    nfaQ :: Set.Set s,
+    nfaI :: s,
+    nfaF :: Set.Set s,
+    nfaD :: Map.Map (s, a) (Set.Set s)
+  }
+
+data Partition a = Partition
+  { partSet :: Set.Set a,
+    partEqc :: a -> Set.Set a
+  }
+
+resolvePartition :: Ord a => Partition a -> Set.Set (Set.Set a)
+resolvePartition part = Set.map (\x -> partEqc part x) $ partSet part
+
+instance (Ord a, Show a) => Show (Partition a) where
+  show = show . resolvePartition
+
+makeConcrete :: Ord a => Partition a -> Partition a
+makeConcrete part =
+  let mapping = Map.fromSet (partEqc part) (partSet part)
+   in part {partEqc = (mapping Map.!)}
+
+forAll :: Foldable t => t a -> (a -> Bool) -> Bool
+forAll a f = foldl' (\c x -> c && f x) True a
+
+nextPartition :: (Ord s, Ord a, Show s) => DFA s a -> Partition s -> Partition s
+nextPartition dfa part =
+  part
+    { partEqc = \q ->
+        Set.filter
+          ( \q' ->
+              forAll (dfaA dfa) $ \a ->
+                partEqc part (dfaD dfa Map.! (q, a)) == partEqc part (dfaD dfa Map.! (q', a))
+          )
+          $ partEqc part q
+    }
+
+instance Ord a => Eq (Partition a) where
+  p1 == p2 = resolvePartition p1 == resolvePartition p2
+
+untilEq :: Eq a => (a -> a) -> a -> a
+untilEq m a = snd $ until (\(x, y) -> x == y) (\(_, y) -> (y, m y)) (a, m a)
+
+partitionFinal :: Ord s => DFA s a -> Partition s
+partitionFinal dfa =
+  let nonAccepting = dfaQ dfa Set.\\ dfaF dfa
+      accepting = dfaF dfa
+      eqc q = case () of
+        ()
+          | q `Set.member` accepting -> accepting
+          | q `Set.member` nonAccepting -> nonAccepting
+          | otherwise -> undefined
+   in Partition {partSet = dfaQ dfa, partEqc = eqc}
+
+minimizeDFA :: (Ord s, Ord a, Show s) => DFA s a -> DFA (Set.Set s) a
+minimizeDFA dfa =
+  let initialPart = partitionFinal dfa
+      part = untilEq (nextPartition dfa . makeConcrete) initialPart
+      q = resolvePartition part
+   in dfa
+        { dfaQ = q,
+          dfaI = partEqc part $ dfaI dfa,
+          dfaF = Set.map (partEqc part) $ dfaF dfa,
+          dfaD =
+            Map.fromSet
+              ( \(p, a) -> let p' = Set.elems p !! 0 in partEqc part $ dfaD dfa Map.! (p', a)
+              )
+              $ Set.cartesianProduct q (dfaA dfa)
+        }
+
+data FA s a = FA {faA :: Set.Set a, faD :: (s, a) -> s}
+
+faFromDFA :: (Ord s, Ord a) => DFA s a -> FA s a
+faFromDFA dfa = FA {faA = dfaA dfa, faD = (dfaD dfa Map.!)}
+
+faStep :: Ord s => FA s a -> Set.Set s -> Set.Set s
+faStep fa =
+  Set.unions
+    . Set.map
+      ( \q ->
+          Set.map (\a -> faD fa (q, a)) $ faA fa
+      )
+
+faMultiStep :: Ord s => FA s a -> Set.Set s -> Set.Set s -> Set.Set s
+faMultiStep fa ss frontier =
+  let frontier' = faStep fa frontier Set.\\ frontier
+   in if frontier' `Set.isSubsetOf` ss
+        then ss
+        else faMultiStep fa (Set.union ss frontier') frontier'
+
+faReachable :: Ord s => FA s a -> s -> Set.Set s
+faReachable fa from = faMultiStep fa froms froms
+  where
+    froms = Set.singleton from
+
+dfaRemoveUnreached :: (Ord s, Ord a) => DFA s a -> DFA s a
+dfaRemoveUnreached dfa =
+  dfa
+    { dfaQ = reached,
+      dfaF = dfaF dfa `Set.intersection` reached
+    }
+  where
+    reached = faReachable (faFromDFA dfa) (dfaI dfa)
+
+toDFA :: (Ord s, Ord a) => NFA s a -> DFA (Set.Set s) a
+toDFA nfa =
+  let d = \(q, a) -> Set.unions $ Set.map (\q' -> fromMaybe (Set.empty) $ nfaD nfa Map.!? (q', a)) q
+      i = Set.singleton $ nfaI nfa
+      q = faReachable (FA {faA = nfaA nfa, faD = d}) i
+      f = Set.filter (\h -> not $ Set.null $ h `Set.intersection` nfaF nfa) q
+   in DFA
+        { dfaA = nfaA nfa,
+          dfaI = i,
+          dfaQ = q,
+          dfaD = Map.fromSet d (Set.cartesianProduct q (nfaA nfa)),
+          dfaF = f
+        }
+
+data NFAL s a = NFAL
+  { nfalA :: Set.Set a,
+    nfalQ :: Set.Set s,
+    nfalI :: s,
+    nfalF :: Set.Set s,
+    nfalD :: Map.Map (s, Maybe a) (Set.Set s)
+  }
+
+lStep :: (Ord s, Ord a) => NFAL s a -> Set.Set s -> Set.Set s
+lStep nfal = Set.unions . Set.map (\q -> nfalD nfal Map.! (q, Nothing))
+
+lMultiStep :: (Ord s, Ord a) => NFAL s a -> Set.Set s -> Set.Set s -> Set.Set s
+lMultiStep nfal ss frontier =
+  let frontier' = lStep nfal frontier Set.\\ frontier
+   in if frontier' `Set.isSubsetOf` ss
+        then ss
+        else lMultiStep nfal (Set.union ss frontier') frontier'
+
+lClosure :: (Ord s, Ord a) => NFAL s a -> s -> Set.Set s
+lClosure nfal s = lMultiStep nfal ss ss
+  where
+    ss = Set.singleton s
+
+toNFA :: (Show s, Show a, Ord s, Ord a) => NFAL s a -> NFA s a
+toNFA nfal =
+  let outgoing = Map.keysSet (nfalD nfal)
+      outgoing' =
+        Set.foldl'
+          ( \out' (q, a) ->
+              case a of
+                Just a' -> Set.insert (q, a') out'
+                Nothing -> out'
+          )
+          Set.empty
+          outgoing
+   in NFA
+        { nfaA = nfalA nfal,
+          nfaQ = nfalQ nfal,
+          nfaI = nfalI nfal,
+          nfaF =
+            Set.filter
+              ( \q ->
+                  not $ Set.null $ lClosure nfal q `Set.intersection` (nfalF nfal)
+              )
+              $ nfalQ nfal,
+          nfaD =
+            Map.unionsWith Set.union $
+              Set.map
+                ( \q ->
+                    let reachableFromQ = lClosure nfal q
+                     in Map.foldrWithKey'
+                          ( \(q', a) q'' total ->
+                              if q' `Set.member` reachableFromQ
+                                then case a of
+                                  Just a' -> Map.insertWith Set.union (q, a') q'' total
+                                  Nothing -> total
+                                else total
+                          )
+                          Map.empty
+                          (nfalD nfal)
+                )
+                (nfalQ nfal)
+        }
+
+data IntGenerator a = IntGenerator {runIntGenerator :: Int -> (Int, a)}
+
+updateSnd :: (b -> c) -> (a, b) -> (a, c)
+updateSnd f (x, y) = (x, f y)
+
+instance Functor IntGenerator where
+  fmap f a = IntGenerator $ (updateSnd f) . (runIntGenerator a)
+
+instance Applicative IntGenerator where
+  pure a = IntGenerator $ \x -> (x, a)
+  liftA2 f ia ib = IntGenerator $ \x ->
+    let (x', a) = runIntGenerator ia x
+        (x'', b) = runIntGenerator ib x'
+     in (x'', f a b)
+
+instance Monad IntGenerator where
+  (>>=) ig f = IntGenerator $ \x ->
+    let (x', a) = runIntGenerator ig x
+     in runIntGenerator (f a) x'
+
+nextInt :: IntGenerator Int
+nextInt = IntGenerator $ \x -> (x + 1, x)
+
+execIntGenerator :: IntGenerator a -> a
+execIntGenerator ig = snd $ runIntGenerator ig 0
+
+nfalDefaultD :: (Ord s, Ord a) => Set.Set s -> Map.Map (s, Maybe a) (Set.Set s)
+nfalDefaultD q = Map.fromSet (Set.singleton . fst) $ Set.map (\p -> (p, Nothing)) $ q
+
+withNFALDefaultD :: (Ord s, Ord a) => Set.Set s -> [Map.Map (s, Maybe a) (Set.Set s)] -> Map.Map (s, Maybe a) (Set.Set s)
+withNFALDefaultD q d = Map.unionsWith Set.union (nfalDefaultD q : d)
+
+toNFAL :: Ord a => RE a -> NFAL Int a
+toNFAL = execIntGenerator . toNFAL_
+
+toNFAL_ :: Ord a => RE a -> IntGenerator (NFAL Int a)
+toNFAL_ RE0 = do
+  q0 <- nextInt
+  let q = Set.singleton q0
+  pure $
+    NFAL
+      { nfalA = Set.empty,
+        nfalQ = q,
+        nfalI = q0,
+        nfalF = Set.empty,
+        nfalD = nfalDefaultD q
+      }
+toNFAL_ RE1 = do
+  q0 <- nextInt
+  let q = Set.singleton q0
+  pure $
+    NFAL
+      { nfalA = Set.empty,
+        nfalQ = q,
+        nfalI = q0,
+        nfalF = q,
+        nfalD = nfalDefaultD q
+      }
+toNFAL_ (REA a) = do
+  q0 <- nextInt
+  f <- nextInt
+  let q = Set.fromList [q0, f]
+  pure $
+    NFAL
+      { nfalA = Set.singleton a,
+        nfalQ = q,
+        nfalI = q0,
+        nfalF = Set.singleton f,
+        nfalD =
+          withNFALDefaultD
+            q
+            [ Map.fromList [((q0, Just a), Set.singleton f)]
+            ]
+      }
+toNFAL_ (REC a b) = do
+  m1 <- toNFAL_ a
+  m2 <- toNFAL_ b
+  q0 <- nextInt
+  f <- nextInt
+  let q1 = nfalI m1
+  let q2 = nfalI m2
+  let f1 = Set.toList (nfalF m1) !! 0
+  let f2 = Set.toList (nfalF m2) !! 0
+  let q = Set.unions [(nfalQ m1), (nfalQ m2), Set.fromList [q0, f]]
+  pure $
+    NFAL
+      { nfalA = (nfalA m1) `Set.union` (nfalA m2),
+        nfalQ = q,
+        nfalI = q0,
+        nfalF = Set.singleton f,
+        nfalD =
+          withNFALDefaultD
+            q
+            [ Map.fromList
+                [ ((q0, Nothing), Set.fromList [q1, q2]),
+                  ((f1, Nothing), Set.singleton f),
+                  ((f2, Nothing), Set.singleton f)
+                ],
+              nfalD m1,
+              nfalD m2
+            ]
+      }
+toNFAL_ (RES a b) = do
+  m1 <- toNFAL_ a
+  m2 <- toNFAL_ b
+  let q1 = nfalI m1
+  let q2 = nfalI m2
+  let f1 = Set.toList (nfalF m1) !! 0
+  let f2 = Set.toList (nfalF m2) !! 0
+  let q = (nfalQ m1) `Set.union` (nfalQ m2)
+  pure $
+    NFAL
+      { nfalA = (nfalA m1) `Set.union` (nfalA m2),
+        nfalQ = q,
+        nfalI = q1,
+        nfalF = Set.singleton f2,
+        nfalD =
+          withNFALDefaultD
+            q
+            [ Map.fromList [((f1, Nothing), Set.singleton q2)],
+              nfalD m1,
+              nfalD m2
+            ]
+      }
+toNFAL_ (REK a) = do
+  m1 <- toNFAL_ a
+  q0 <- nextInt
+  let q1 = nfalI m1
+  let f1 = Set.toList (nfalF m1) !! 0
+  let q = Set.insert q0 $ nfalQ m1
+  pure $
+    NFAL
+      { nfalA = nfalA m1,
+        nfalQ = q,
+        nfalI = q0,
+        nfalF = Set.singleton q0,
+        nfalD =
+          withNFALDefaultD
+            q
+            [ Map.fromList [((f1, Nothing), Set.singleton q0), ((q0, Nothing), Set.singleton q1)],
+              nfalD m1
+            ]
+      }
+
+renumerateDFA :: (Ord s, Ord a) => DFA s a -> DFA Int a
+renumerateDFA dfa =
+  let q = Set.toAscList $ dfaQ dfa
+      mapping = Map.fromAscList $ zip q [0 ..]
+      numberOf = (mapping Map.!)
+   in DFA
+        { dfaA = dfaA dfa,
+          dfaQ = Set.map numberOf $ dfaQ dfa,
+          dfaI = mapping Map.! dfaI dfa,
+          dfaF = Set.map numberOf $ dfaF dfa,
+          dfaD = Map.map numberOf $ Map.mapKeys (\(q, a) -> (numberOf q, a)) (dfaD dfa)
+        }
+
+dfaOutgoing :: (Ord s, Ord a) => DFA s a -> s -> Map.Map s (Set.Set a)
+dfaOutgoing dfa q =
+  Set.foldr'
+    ( \a ->
+        Map.alter (Just . maybe (Set.singleton a) (Set.insert a)) (dfaD dfa Map.! (q, a))
+    )
+    Map.empty
+    $ dfaA dfa
+
+nfalOutgoing :: (Ord s, Ord a) => NFAL s a -> s -> Map.Map s (Set.Set (Maybe a))
+nfalOutgoing nfal q =
+  Set.foldr'
+    ( \a total ->
+        case (nfalD nfal Map.!? (q, a)) of
+          Just statesForChar ->
+            Set.foldr'
+              ( Map.alter (Just . maybe (Set.singleton a) (Set.insert a))
+              )
+              total
+              statesForChar
+          Nothing ->
+            total
+    )
+    Map.empty
+    $ Set.insert Nothing (Set.map Just (nfalA nfal))
+
+nfaOutgoing :: (Ord s, Ord a) => NFA s a -> s -> Map.Map s (Set.Set a)
+nfaOutgoing nfa q =
+  Set.foldr'
+    ( \a total ->
+        case (nfaD nfa Map.!? (q, a)) of
+          Just statesForChar ->
+            Set.foldr'
+              ( Map.alter (Just . maybe (Set.singleton a) (Set.insert a))
+              )
+              total
+              statesForChar
+          Nothing ->
+            total
+    )
+    Map.empty
+    $ nfaA nfa
+
+instance (Show s, Show a, Ord s, Ord a) => Show (NFAL s a) where
+  show nfal = execWriter $ do
+    tell "digraph {\n  rankdir=\"LR\";\n  start [shape=point];\n"
+    forM_ (nfalQ nfal) $ \q -> do
+      tell "  "
+      tell $ show q
+      if q `Set.member` nfalF nfal
+        then tell $ " [shape=doublecircle]"
+        else tell $ " [shape=circle]"
+      tell ";\n"
+    forM_ (nfalQ nfal) $ \q -> do
+      let outgoing = nfalOutgoing nfal q
+      forM_ (Map.keys outgoing) $ \q' -> do
+        tell "  "
+        tell $ show q
+        tell " -> "
+        tell $ show q'
+        tell " [label="
+        tell $ show $ intercalate ", " $ map show $ Set.toList $ outgoing Map.! q'
+        tell "]"
+        tell ";\n"
+    tell "  start -> "
+    tell $ show (nfalI nfal)
+    tell ";\n"
+    tell "}"
+
+instance (Show s, Show a, Ord s, Ord a) => Show (NFA s a) where
+  show nfa = execWriter $ do
+    tell "digraph {\n  rankdir=\"LR\";\n  start [shape=point];\n"
+    forM_ (nfaQ nfa) $ \q -> do
+      tell "  "
+      tell $ show q
+      if q `Set.member` nfaF nfa
+        then tell $ " [shape=doublecircle]"
+        else tell $ " [shape=circle]"
+      tell ";\n"
+    forM_ (nfaQ nfa) $ \q -> do
+      let outgoing = nfaOutgoing nfa q
+      forM_ (Map.keys outgoing) $ \q' -> do
+        tell "  "
+        tell $ show q
+        tell " -> "
+        tell $ show q'
+        tell " [label="
+        tell $ show $ intercalate ", " $ map show $ Set.toList $ outgoing Map.! q'
+        tell "]"
+        tell ";\n"
+    tell "  start -> "
+    tell $ show (nfaI nfa)
+    tell ";\n"
+    tell "}"
+
+instance (Show s, Show a, Ord s, Ord a) => Show (DFA s a) where
+  show dfa = execWriter $ do
+    tell "digraph {\n  rankdir=\"LR\";\n  start [shape=point];\n"
+    forM_ (dfaQ dfa) $ \q -> do
+      tell "  "
+      tell $ show q
+      if q `Set.member` dfaF dfa
+        then tell $ " [shape=doublecircle]"
+        else tell $ " [shape=circle]"
+      tell ";\n"
+    forM_ (dfaQ dfa) $ \q -> do
+      let outgoing = dfaOutgoing dfa q
+      forM_ (Map.keys outgoing) $ \q' -> do
+        tell "  "
+        tell $ show q
+        tell " -> "
+        tell $ show q'
+        tell " [label="
+        tell $ show $ intercalate ", " $ map show $ Set.toList $ outgoing Map.! q'
+        tell "]"
+        tell ";\n"
+    tell "  start -> "
+    tell $ show (dfaI dfa)
+    tell ";\n"
+    tell "}"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  when (length args /= 2) $ do
+    putStrLn "Expected exactly two arguments: fatest <regex> <format>"
+    putStrLn "Where <format> is one of [re, nfal, nfa, dfa, mdfa]"
+    exitFailure
+
+  let reStr = args !! 0
+  re <- case (parseStr reStr) of
+    Left errMsg -> do
+      putStrLn $ "Could not parse the provided regex " ++ show reStr
+      putStrLn $ "(Error: " ++ errMsg ++ ")"
+      exitFailure
+    Right tree ->
+      pure tree
+  let nfal = toNFAL re
+  let nfa = toNFA nfal
+  let dfa = renumerateDFA $ toDFA nfa
+  let mdfa = renumerateDFA $ minimizeDFA dfa
+  putStrLn $ case args !! 1 of
+    "re" -> show re
+    "nfal" -> show nfal
+    "nfa" -> show nfa
+    "dfa" -> show dfa
+    "mdfa" -> show mdfa
+    otherwise -> "Unknown format " ++ show (args !! 1)
---
+++ app/Text/Lare.hs
@@ -0,0 +1,158 @@
+module Text.Lare (RE (..), parseStr) where
+
+import Control.Applicative
+import Control.Monad
+import Data.Char
+
+newtype Parser a = Parser {parse :: String -> [(a, String)]}
+
+runParser :: Parser a -> String -> Either String a
+runParser m s =
+  case parse m s of
+    [(res, [])] -> Right res
+    [(_, rs)] -> Left "parser did not consume entire stream"
+    _ -> Left "parser error"
+
+item :: Parser Char
+item = Parser $ \s ->
+  case s of
+    [] -> []
+    (c : cs) -> [(c, cs)]
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+bind p f = Parser $ \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
+
+unit :: a -> Parser a
+unit a = Parser $ \s -> [(a, s)]
+
+instance Functor Parser where
+  fmap f (Parser cs) = Parser $ \s -> [(f a, b) | (a, b) <- cs s]
+
+instance Applicative Parser where
+  pure = unit
+  (Parser cs1) <*> (Parser cs2) = Parser $ \s -> [(f a, s2) | (f, s1) <- cs1 s, (a, s2) <- cs2 s1]
+
+instance Monad Parser where
+  return = pure
+  (>>=) = bind
+
+instance MonadPlus Parser where
+  mzero = failure
+  mplus = combine
+
+instance Alternative Parser where
+  empty = mzero
+  (<|>) = option
+
+combine :: Parser a -> Parser a -> Parser a
+combine p q = Parser $ \s -> parse p s ++ parse q s
+
+failure :: Parser a
+failure = Parser $ const []
+
+option :: Parser a -> Parser a -> Parser a
+option p q = Parser $ \s ->
+  case parse p s of
+    [] -> parse q s
+    res -> res
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p =
+  item >>= \c ->
+    if p c
+      then unit c
+      else failure
+
+oneOf :: [Char] -> Parser Char
+oneOf s = satisfy (flip elem s)
+
+chainl :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
+chainl p op a = (p `chainl1` op) <|> return a
+
+chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
+p `chainl1` op = do
+  a <- p
+  rest a
+  where
+    rest a =
+      ( do
+          f <- op
+          b <- p
+          rest $ f a b
+      )
+        <|> return a
+
+char :: Char -> Parser Char
+char c = satisfy (c ==)
+
+alph :: Parser (RE Char)
+alph = REA <$> satisfy isAlpha
+
+token :: Parser a -> Parser a
+token p = do
+  spaces
+  a <- p
+  spaces
+  return a
+
+string :: String -> Parser String
+string [] = return []
+string s@(c : cs) = do
+  char c
+  string cs
+  return s
+
+reserved :: String -> Parser String
+reserved = token . string
+
+spaces :: Parser String
+spaces = many $ oneOf " \n\r\t"
+
+parens :: Parser a -> Parser a
+parens m = do
+  reserved "("
+  n <- m
+  reserved ")"
+  return n
+
+zero :: Parser (RE Char)
+zero = reserved "0" >> return RE0
+
+one :: Parser (RE Char)
+one = reserved "1" >> return RE1
+
+expr :: Parser (RE Char)
+expr = choice
+
+choice :: Parser (RE Char)
+choice = multiplication `chainl1` choiceOp
+
+multiplication :: Parser (RE Char)
+multiplication = quantification `chainl1` mulOp
+
+quantification :: Parser (RE Char)
+quantification =
+  ( do
+      val <- value
+      reserved "*"
+      return $ REK val
+  )
+    <|> value
+
+value :: Parser (RE Char)
+value = alph <|> zero <|> one <|> parens expr
+
+infixOp :: String -> (a -> a -> a) -> Parser (a -> a -> a)
+infixOp x f = reserved x >> return f
+
+choiceOp :: Parser (RE Char -> RE Char -> RE Char)
+choiceOp = infixOp "+" REC
+
+mulOp :: Parser (RE Char -> RE Char -> RE Char)
+mulOp = infixOp "" RES
+
+parseStr :: String -> Either String (RE Char)
+parseStr = runParser expr
+
+data RE a = RE0 | RE1 | REA a | REC (RE a) (RE a) | RES (RE a) (RE a) | REK (RE a)
+  deriving (Show)
